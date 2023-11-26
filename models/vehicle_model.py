998675a5d15457903ea4e009d58038399ed9e530
Gary Yaral <gary.yaral@gmail.com>
2023-11-26 12:56:44 -0500

    Se añadió metodo para cargar datos d formulario de registro

--- models/vehicle_model.py
+++ models/vehicle_model.py
@@ -4,22 +4,25 @@ from db_constants.common_functions import closeConnection, openConnection
 class Vehicle():
   conn = None
   cursor = None
-
+  def __init__(self,):
+    db = MysqlDB()
+    self.conn = db.connect()
+  
+  def new_cursor(self):
+    return self.conn.cursor(dictionary=True)
+  
   # Funcion que hace la busqueda de la placa en la BD
   def find_vehicle(self, plate_number):
-    openConnection(self, MysqlDB)
+    cursor = self.new_cursor()
     try:
       query = """
           SELECT 
             vehicles.id AS id, 
             plate_number,
             status_type_id,
-            status_type.name AS status_type_name,
             access_type_id,
-            access_type.name AS access_type_name,
             status_type_id,
-            status_type.name AS status_type_name
-            
+            vehicles_type.id AS vehicle_type_id
           FROM vehicles
           INNER JOIN vehicles_type
           ON vehicles_type.id = vehicles.vehicle_type_id
@@ -29,15 +32,90 @@ class Vehicle():
           ON access_type.id = vehicles.access_type_id
           WHERE vehicles.plate_number=%s"""
       params = (plate_number,)
-      self.cursor.execute(query, params)
-      result = self.cursor.fetchone()
+      cursor.execute(query, params)
+      result = cursor.fetchone()
       if result == None:
         return (False, None)
       return (True, result)
     except Exception as e:
       print("Error: {}".format(e))
+      return (False, None)
     finally:
-      closeConnection(self)
+      cursor.close()
 
+  def load_access_type(self):
+    cursor = self.new_cursor()
+    try:
+      query = """
+          SELECT * from access_type
+            """
+      cursor.execute(query)
+      result = cursor.fetchall()
+      if result == None:
+        return {"loaded": False }
+      return {"loaded": False, "data": result}
+    except Exception as e:
+      print("Error: {}".format(e))
+      {"loaded": False }
+    finally:
+      cursor.close()
+
+  def load_status_type(self):
+    cursor = self.new_cursor()
+    try:
+      query = """
+          SELECT * from status_type
+            """
+      cursor.execute(query)
+      result = cursor.fetchall()
+      if result == None:
+        {"loaded": False }
+      return {"loaded": False, "data": result}
+    except Exception as e:
+      print("Error: {}".format(e))
+      {"loaded": False }
+    finally:
+      cursor.close()
+
+  def load_vehicles_type(self):
+    cursor = self.new_cursor()
+    try:
+      query = """
+          SELECT * from vehicles_type
+            """
+      cursor.execute(query)
+      result = cursor.fetchall()
+      if result == None:
+        {"loaded": False }
+      return {"loaded": False, "data": result}
+    except Exception as e:
+      print("Error: {}".format(e))
+      {"loaded": False }
+    finally:
+      cursor.close()
+  
+  def load_required_data(self):
+    try:
+      access_type = self.load_access_type()
+      status_type = self.load_status_type()
+      vehicles_type = self.load_vehicles_type()
+      data = {
+        "vehicles_type": (),
+        "status_type": (),
+        "access_type": ()
+      } 
+      if access_type["loaded"]:
+        data["access_type"] = access_type["data"]
+      
+      if status_type["loaded"]:
+        data["status_type"] = status_type["data"]
+
+      if vehicles_type["loaded"]:
+        data["vehicles_type"] = vehicles_type["data"]
+
+      return (True, data)
+    except Exception as e:
+      print("Error: {}".format(e))
+      return (False, None)
  
 VehicleModel = Vehicle()
